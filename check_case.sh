1449bb3ba969d0d46efcbddf765bb7bc4d1ce693
shanmathi <brawny100420@gmail.com>
2025-11-11 07:44:58 +0530

    Fix condition to check if names are different

--- check_case.sh
+++ check_case.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p  "enter the name" name1
 read -p  "enter the name" name2
-if [ "$name1" = "$name2" ];then 
+if [ "$name1" != "$name2" ];then 
 	echo "they both or same name"
 else 
 	echo "they both or not same"
